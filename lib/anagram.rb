2cfec06d51c0d75c52c5bdb7c7995cd1ca2c2fe2
Marcel Rico <personal.mr4@gmail.com>
2020-01-04 15:42:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    result = []
+    word_letters = @word.split("").sort
+    array.each do |word|
+      converted = word.split("").sort
+      test1 = (word_letters - converted).length == 0
+      test2 = word_letters.length == converted.length
+      if test1 && test2
+        result << word
+      end
+    end
+    result
+  end
+end
