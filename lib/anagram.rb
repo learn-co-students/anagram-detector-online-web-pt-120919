5706271a634352c1a91e8aae6ac4403ea7e329b7
Asija Watson <asija.w@yahoo.com>
2020-01-12 21:28:28 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :keyword
+  def initialize(keyword)
+    @keyword = keyword
+  end
+  
+  def match(word_list)
+    match_list = []
+    word_list.each {|word| 
+      if @keyword.split("").sort == word.split("").sort
+        match_list << word
+      end
+    }
+    match_list
+  end
+end
+    
