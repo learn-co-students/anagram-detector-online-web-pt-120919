e74170a66e40e066cef6411bd5e20a1649d1f95a
Kenneth Neylon <kenneth_neylon@hotmail.com>
2020-01-16 05:20:07 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+    
+  def match(find_anagram_in_this_string) 
+    puts "Must find #{self.word} inside the following string: #{find_anagram_in_this_string}"
+    find_anagram_in_this_string.find_all do |word| 
+      if word.split("").sort == self.word.split("").sort 
+        word
+      end
+    end 
+  end
+  
+    
+    
+    
+    
+    
+end  
